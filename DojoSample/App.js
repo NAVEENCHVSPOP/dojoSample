5717f1c53924a430894083d5fbab778dba895c77
Naveen Chintalapudi <naveen.chintalapudi@popcornapps.com>
2023-11-03 15:40:41 +0530

    Apple pay functional code

--- DojoSample/App.js
+++ DojoSample/App.js
@@ -8,22 +8,46 @@
 
 import React from 'react';
 import {View, StyleSheet, TouchableOpacity, Text, Alert} from 'react-native';
-import {startSetupFlow} from '@dojo-engineering/react-native-pay-sdk';
+import {
+  startSetupFlow,
+  startPaymentFlow,
+} from '@dojo-engineering/react-native-pay-sdk';
 
 const App = () => {
   const handleCheckout = () => {
     startSetupFlow({
-      intentId: 'si_sandbox_6ZslI6tkLkWbubqlASahEw',
+      intentId: 'si_sandbox_ctAf6U4pNkqmMRx0UQ7W6w',
     }).then(res => {
-      Alert.alert(`Result: ` + res);
-      console.log(`Result: ` + res);
+      Alert.alert(`Setup intent result: ` + res);
+      console.log(`Setup intent result: ` + res);
     });
   };
+
+  const handlePaymentIntentPress = () => {
+    startPaymentFlow({
+      intentId: 'pi_sandbox_dcWwbxf6sUGU5yKe0MlZ5g',
+      applePayMerchantId: 'merchant.com.derwentlondon.dl.sandbox',
+    }).then(res => {
+      Alert.alert(`Payment intent result: ` + res);
+      console.log(`Payment intent result: ` + res);
+    });
+  };
+
+  // function getAppleMerchantId() {
+  //   let merchantId = `merchant.com.derwentlondon.dl.sandbox`;
+  //   return merchantId;
+  // }
 
   return (
     <View style={styles.mainViewStyle}>
       <TouchableOpacity style={styles.btnViewStyle} onPress={handleCheckout}>
-        <Text style={styles.txtStyle}>Checkout</Text>
+        <Text style={styles.txtStyle}>Setup Flow</Text>
+      </TouchableOpacity>
+
+      <TouchableOpacity
+        style={styles.btnView2Style}
+        onPress={handlePaymentIntentPress}>
+        <Text style={styles.txtStyle}>Payment Flow</Text>
       </TouchableOpacity>
     </View>
   );
@@ -36,6 +60,14 @@ const styles = StyleSheet.create({
     alignItems: 'center',
   },
   btnViewStyle: {
+    width: 120,
+    height: 50,
+    backgroundColor: 'blue',
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  btnView2Style: {
+    marginTop: 20,
     width: 120,
     height: 50,
     backgroundColor: 'blue',
